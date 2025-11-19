a4068d7a424ac3a77fcfd86b18bfba49ea543294
Scott McEvilly <smcevilly@bloomfieldhillsrobotics.org>
2025-11-18 23:22:43 -0500

    Tuning for PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlyWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlyWheel.java
@@ -15,6 +15,9 @@ import dev.nextftc.hardware.impl.MotorEx;
 @Configurable
 
 public class FlyWheel implements Subsystem {
+    public static double targetspeed = 2400;
+//    public static double kP = 0.00035, kI = 1E-7, kD = 0, kV = 1.667E-4, kA = 0, kS = 0.00425;
+    public static double kP = 1.775E-4, kI = 1.5E-18, kD = 0, kV = 0, kA = 0, kS = 0;
     public static final FlyWheel INSTANCE = new FlyWheel();
     private FlyWheel() { }
     MotorGroup FlyWheelGroup = new MotorGroup(
@@ -22,20 +25,19 @@ public class FlyWheel implements Subsystem {
             new MotorEx("FlywheelLeft").reversed()
     );
 
-    public static double kP = 1.6667E-4, kI = 0, kD = 0, kV = 0.00038, kA = 0, kS = 0.0425;
 
-    public final PIDCoefficients pid = new PIDCoefficients(1.6667E-4,0,0);
+    public final PIDCoefficients pid = new PIDCoefficients(kP,kI,kD);
     public final BasicFeedforwardParameters ff =
-            new BasicFeedforwardParameters(0.00038, 0, 0.0425);
+            new BasicFeedforwardParameters(kV,kA,kS);
 
     private final ControlSystem controller = ControlSystem.builder()
             .velPid(pid)
             .basicFF(ff)
             .build();
 
-    public Command superlongshot = new RunToVelocity(controller, 750).requires(this);
-    public Command longshot = new RunToVelocity(controller, 500).requires(this);
-    public Command shortshot     = new RunToVelocity(controller, 350).requires(this);
+    public Command superlongshot = new RunToVelocity(controller, targetspeed*1.25).requires(this);
+    public Command longshot = new RunToVelocity(controller, targetspeed).requires(this);
+    public Command shortshot     = new RunToVelocity(controller, targetspeed * .8).requires(this);
     public final Command stop = new InstantCommand(() -> controller.setGoal(new KineticState(0,0))).requires(this);
 
     @Override
