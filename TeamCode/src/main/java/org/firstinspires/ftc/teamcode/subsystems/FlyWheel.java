2f6d8a80a84b581791bc1f9c3d44e49dacc450bf
thomaslotz <thomas.lotz@gmail.com>
2025-11-20 08:52:02 -0500

    implemented pid_flywheel in troubleshooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlyWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlyWheel.java
@@ -1,3 +1,4 @@
+/*
 package org.firstinspires.ftc.teamcode.subsystems;
 import com.bylazar.configurables.annotations.Configurable;
 import dev.nextftc.control.ControlSystem;
@@ -46,7 +47,54 @@ public class FlyWheel implements Subsystem {
 //    public Command longshot = new SetPower(FlyWheelGroup,.75).requires(this);
 //    public Command shortshot = new SetPower(FlyWheelGroup,.6).requires(this);
 //    public Command stop = new SetPower(FlyWheelGroup,0).requires(this);
-    @Override
+
+
+ */
+
+package org.firstinspires.ftc.teamcode.subsystems;
+import com.bylazar.configurables.annotations.Configurable;
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
+import dev.nextftc.control.feedback.PIDCoefficients;
+import dev.nextftc.control.feedforward.BasicFeedforwardParameters;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.hardware.controllable.MotorGroup;
+import dev.nextftc.hardware.controllable.RunToVelocity;
+import dev.nextftc.hardware.impl.MotorEx;
+
+@Configurable
+
+public class FlyWheel implements Subsystem {
+    public static final FlyWheel INSTANCE = new FlyWheel();
+    public static double tolerance = 50;
+    public static double maxvelocity = 2800;
+    private FlyWheel() { }
+    MotorGroup FlyWheelGroup = new MotorGroup(
+            new MotorEx("FlywheelRight"),
+            new MotorEx("FlywheelLeft").reversed()
+    );
+
+    public static double kP = 1.6667E-4, kI = 0, kD = 0, kV = 0.00038, kA = 0, kS = 0.0425;
+
+    public final PIDCoefficients pid = new PIDCoefficients(kP, kI, kD);
+    public final BasicFeedforwardParameters ff =
+            new BasicFeedforwardParameters(kV, kA, kS);
+
+    private final ControlSystem controller = ControlSystem.builder()
+            .velPid(pid)
+            .basicFF(ff)
+            .build();
+
+    public Command superlongshot = new RunToVelocity(controller, maxvelocity, tolerance).requires(this);
+    public Command longshot = new RunToVelocity(controller, 0.7*maxvelocity, tolerance).requires(this);
+    public Command shortshot     = new RunToVelocity(controller, 0.5*maxvelocity, tolerance).requires(this);
+    public final Command stop = new InstantCommand(() -> controller.setGoal(new KineticState(0,0))).requires(this);
+
+
+@Override
     public void periodic() {
 //        FlyWheelGroup.setPower(controller.calculate(FlyWheelGroup.getState()));
         ActiveOpMode.telemetry().addData("Flywheel State", FlyWheelGroup.getState());
