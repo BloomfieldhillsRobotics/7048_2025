158e633746ab76df9f859887eea497f90b2af2e0
Scott McEvilly <smcevilly@bloomfieldhillsrobotics.org>
2025-12-02 23:54:39 -0500

    Pose Corrections

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/AutoPoses.java
@@ -4,29 +4,29 @@ import com.pedropathing.geometry.Pose;
 
 public class AutoPoses {
     public static class BlueShort {
-        public static final Pose startPose = new Pose(27.5, 132.3, Math.toRadians(330));
-        public static final Pose scanPose = new Pose(83, 118, Math.toRadians(90));
-        public static final Pose scoring1 = new Pose(28, 115, Math.toRadians(135));
-        public static final Pose scoring2 = new Pose(48, 70, Math.toRadians(135));
+        public static final Pose startPose = new Pose(36.2, 119.8, Math.toRadians(322));
+        public static final Pose scanPose = new Pose(72, 120, Math.toRadians(90));
+        public static final Pose scoring1 = new Pose(54, 90, Math.toRadians(135));
+        public static final Pose scoring2 = new Pose(52, 52, Math.toRadians(135));
         public static final Pose pickup1PPG = new Pose(46, 83, Math.toRadians(180));
         public static final Pose pickup2PPG = new Pose(19, 83, Math.toRadians(180));
         public static final Pose pickup1PGP = new Pose(46, 60, Math.toRadians(180));
         public static final Pose pickup2PGP = new Pose(19, 60, Math.toRadians(180));
-        public static final Pose pickup1GPP = new Pose(46, 36, Math.toRadians(180));
-        public static final Pose pickup2GPP = new Pose(19, 36, Math.toRadians(180));
+        public static final Pose pickup1GPP = new Pose(46, 39, Math.toRadians(180));
+        public static final Pose pickup2GPP = new Pose(19, 39, Math.toRadians(180));
     }
 
     public static class RedShort {
-        public static final Pose startPose = new Pose(121.8, 134, Math.toRadians(210));
-        public static final Pose scanPose = new Pose(83,118, Math.toRadians(90));
-        public static final Pose scoring1 = new Pose(116, 115, Math.toRadians(45));
+        public static final Pose startPose = new Pose(121.8, 134, Math.toRadians(217));
+        public static final Pose scanPose = new Pose(72,120, Math.toRadians(90));
+        public static final Pose scoring1 = new Pose(106, 105, Math.toRadians(45));
         public static final Pose scoring2 = new Pose(96, 49, Math.toRadians(45));
-        public static final Pose pickup1PPG = new Pose(100, 10, Math.toRadians(0));
-        public static final Pose pickup2PPG = new Pose(124, 83, Math.toRadians(0));
+        public static final Pose pickup1PPG = new Pose(100, 84, Math.toRadians(0));
+        public static final Pose pickup2PPG = new Pose(124, 84, Math.toRadians(0));
         public static final Pose pickup1PGP = new Pose(100, 60, Math.toRadians(0));
         public static final Pose pickup2PGP = new Pose(124, 60, Math.toRadians(0));
-        public static final Pose pickup1GPP = new Pose(100, 36, Math.toRadians(0));
-        public static final Pose pickup2GPP = new Pose(124, 36, Math.toRadians(0));
+        public static final Pose pickup1GPP = new Pose(100, 35, Math.toRadians(0));
+        public static final Pose pickup2GPP = new Pose(124, 35, Math.toRadians(0));
     }
 
     public static class RedLong {
@@ -38,8 +38,8 @@ public class AutoPoses {
         public static final Pose pickup2PPG = new Pose(124, 84, Math.toRadians(0));
         public static final Pose pickup1PGP = new Pose(100, 60, Math.toRadians(0));
         public static final Pose pickup2PGP = new Pose(124, 60, Math.toRadians(0));
-        public static final Pose pickup1GPP = new Pose(100, 38, Math.toRadians(0));
-        public static final Pose pickup2GPP = new Pose(124, 38, Math.toRadians(0));
+        public static final Pose pickup1GPP = new Pose(100, 35, Math.toRadians(0));
+        public static final Pose pickup2GPP = new Pose(124, 35, Math.toRadians(0));
     }
 
     public static class BlueLong {
